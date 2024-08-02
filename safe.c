aac26520160e68da54ab45f592b6edb20b4d80eb
Julius Vander Arend <julius.vanderarend@student.manchester.ac.uk>
2024-08-02 17:52:51 +0100

    add a new safe c file

---
+++ safe.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(int argc, char *argv[]) {
+    printf("running");
+    srand(time(NULL));
+    int num = rand();
+    printf("number: %d",num);
+    char buffer[10];
+
+    if(num < 10){
+        return 0;
+    }
+a
+    for (int i = 0; i < num; i++) {
+        buffer[i] = 'X';
+    }
+    buffer[num] = '\0';
+
+
+    printf("Buffer contents: %s\n", buffer);
+    return 0;
+}
