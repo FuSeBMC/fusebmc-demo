3105926516f01a2f1393cce851ccfc116ada65b1
kaled Alshmrany <kaa14ed@gmail.com>
2025-04-23 12:29:00 +0100

    Update to unsafe_code.c

---
+++ unsafe_code.c
@@ -0,0 +1,17 @@
+int main(){
+    int a, x, y = 0, z;
+    a = __VERIFIER_nondet_uint();
+    x = __VERIFIER_nondet_uint();
+    z = __VERIFIER_nondet_uint();
+    if (a > 25000 && x == 30000){
+        while (y++ < z) {
+            if (y % 3 != 0) {
+                a++; x--;
+                }
+            else{
+                a--; x++;
+                }
+            assert (a != x);
+        }
+    }
+}
