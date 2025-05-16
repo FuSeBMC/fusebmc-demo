38ca1cf512c8e88f6855e6389447cff4369e6bb0
kaled Alshmrany <kaa14ed@gmail.com>
2025-05-16 13:14:36 +0100

    Update case2.c

--- case2.c
+++ case2.c
@@ -1,6 +1,7 @@
 // This file is part of the SV-Benchmarks collection of verification tasks:
 // https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks
 //
+//
 // SPDX-FileCopyrightText: 2022 SoftSec Lab
 // 
 // SPDX-License-Identifier: MIT
